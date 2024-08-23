0d2bf093f1a69dc024420bc461b5187b0d6b3e10
ATILA395 <atilasantosdemello@gmail.com>
2024-08-23 20:22:46 -0300

    ("O resultado de Atila É",)

---
+++ printAtila.py
@@ -0,0 +1,12 @@
+1! a=10
+   b=20
+   print(“o valor de a é:”  , a)
+   print(“o valor de b é: “ , b)
+
+  auxiliar = a
+  a = b
+  b = auxiliar
+
+  print(“o valor de a é:”  , a)
+  print(“o valor de b é:”  , b)
+	
