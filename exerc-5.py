c64bf1e506ed9a65db24c96571ee4a4b47f3b297
ATILA395 <atilasantosdemello@gmail.com>
2024-08-30 19:35:48 -0300

    Create exerc-5.py

---
+++ exerc-5.py
@@ -0,0 +1,8 @@
+# Lê um valor do teclado
+valor = float(input("Digite um número: "))
+
+# Calcula o antecessor
+antecessor = valor - 1
+
+# Exibe o antecessor
+print(f"O antecessor de {valor} é {antecessor}.")
