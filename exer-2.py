12c1654791778a04172cff743b2390721885249e
ATILA395 <atilasantosdemello@gmail.com>
2024-08-30 19:40:03 -0300

    Create exer-2.py

---
+++ exer-2.py
@@ -0,0 +1,9 @@
+# Leitura dos números
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+# Cálculo da soma
+soma = numero1 + numero2
+
+# Exibição do resultado
+print(f"A soma dos números é {soma}.")
