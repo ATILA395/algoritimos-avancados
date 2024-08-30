169528de9deca2d718d9c6da627549990385934a
ATILA395 <atilasantosdemello@gmail.com>
2024-08-30 19:37:26 -0300

    Create exer-6.py

---
+++ exer-6.py
@@ -0,0 +1,9 @@
+# Lê a base e a altura do retângulo do teclado
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+
+# Calcula a área do retângulo
+area = base * altura
+
+# Exibe a área do retângulo
+print(f"A área do retângulo é {area}.")
