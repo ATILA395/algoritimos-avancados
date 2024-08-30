5ef76e75daecd6e1c148d35b920218db66195764
ATILA395 <atilasantosdemello@gmail.com>
2024-08-30 19:23:32 -0300

    Create exec-1.py

---
+++ exec-1.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+
+print("O valor de a é:", a)
+print("O valor de b é:", b)
+
+# Troca os valores de a e b
+auxiliar = a
+a = b
+b = auxiliar
+
+print("O valor de a é:", a)
+print("O valor de b é:", b)
