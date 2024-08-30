5471f211055b31dbdf672d48d30912cdcabee65d
ATILA395 <atilasantosdemello@gmail.com>
2024-08-30 20:25:23 -0300

    Update exer-3.py

--- exer-3.py
+++ exer-3.py
@@ -1,3 +1,27 @@
-A ← 4/2+2/4		A ← 4/2+2/4
-B ← 4/(2+2)/4		B ← 4/2+2/4
-C ← 4+2*2-4		C ← 4+2*2-4
+# Primeiro par de instruções
+
+# Instrução 1
+resultado1 = (4/2) + (2/4)
+print("Resultado da instrução 1:", resultado1)
+
+# Instrução 2
+A = 4/2 + 2/4
+print("Resultado da instrução 2:", A)
+# Segundo par de instruções
+
+# Instrução 1
+B1 = 4/(2+2)/4
+print("Resultado da instrução 1:", B1)
+
+# Instrução 2
+B2 = 4/2 + 2/4
+print("Resultado da instrução 2:", B2)
+# Terceiro par de instruções
+
+# Instrução 1
+C1 = (4+2)*2 - 4
+print("Resultado da instrução 1:", C1)
+
+# Instrução 2
+C2 = 4 + 2*2 - 4
+print("Resultado da instrução 2:", C2)
