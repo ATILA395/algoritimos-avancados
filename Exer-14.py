47d9bb4bb45de156f0434330a4c9d45548ffb6a2
ATILA395 <atilasantosdemello@gmail.com>
2024-09-02 18:49:33 -0300

    Create Exer-14.py

---
+++ Exer-14.py
@@ -0,0 +1,8 @@
+# Ler um valor do usuário
+valor = float(input("Digite um valor: "))
+
+# Verificar se o valor é maior que 10
+if valor > 10:
+    print("É MAIOR QUE 10!")
+else:
+    print("NÃO É MAIOR QUE 10!")
