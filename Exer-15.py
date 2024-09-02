5d79bfc238e114808b954a49fb5f85ad5964dcfe
ATILA395 <atilasantosdemello@gmail.com>
2024-09-02 18:52:09 -0300

    Create Exer-15.py

---
+++ Exer-15.py
@@ -0,0 +1,8 @@
+# Ler um valor do usuário
+valor = float(input("Digite um valor: "))
+
+# Verificar se o valor é positivo (incluindo zero) ou negativo
+if valor >= 0:
+    print("O valor é positivo.")
+else:
+    print("O valor é negativo.")
