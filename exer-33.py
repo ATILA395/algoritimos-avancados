abe8d6a6444c696bc6f658839347fc9c00e676cc
Átila <atilasantosdemello@gmail.com>
2024-09-28 11:59:26 -0300

    Create exer-33.py

---
+++ exer-33.py
@@ -0,0 +1,11 @@
+# Ler dois valores
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Comparar os números e imprimir a mensagem correspondente
+if numero1 == numero2:
+    print("Números iguais")
+elif numero1 > numero2:
+    print("Primeiro é maior")
+else:
+    print("Segundo maior")
